b766ae396fb44565eca5004d02bc810e6720474d
chparmley <charleshparmley@icloud.com>
2022-08-10 01:44:48 -0500

    Iteration speed increased by only loading cookies on first run. Major refactor--removed spaghetti

--- auto_pickup_7shifts.py
+++ auto_pickup_7shifts.py
@@ -1,36 +1,38 @@
-
+import os
 import time
 import pickle
+
 import selenium.webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import telegram_bot
+from dotenv import load_dotenv
+from pprint import pprint
+
+import twilio_sms
 
 #*************************************
 
 class Shift_Grabber:
-	def __init__(self):
-		self.SHIFT_POOL_URL = "https://app.7shifts.com/company/139871/shift_pool/up_for_grabs"
+	def __init__(self, shift_pool_url, CONFIRM_PICKUP_BUTTON):
+		self.shift_pool_url = shift_pool_url
 		self.shift_table_selector = "._1d8Ci"
 		self.shift_details_selector = "._OTcMc"
 		self.shift_pickup_button = 'button'
-		self.confirm_pickup_button = '/html/body/div[12]/div/div/div/div[3]/div/button[1]'
-
-
+		self.CONFIRM_PICKUP_BUTTON = CONFIRM_PICKUP_BUTTON
 		self.shift_wanted = {
+			'location':'Yard Bar Westport',
 			'position':'Security',
 			'day':'Sun',
-			'time':'4:00PM-11:00PM'
+			'time':['4:00PM','11:00PM']
 		}
-
 		self.shift_taken = False
-		self.telegram_bot_message = ""
-		self.debug = True
-		self.driver = self.setup_webdriver()
-		
+		self.headless = True
+		self.driver = None
+		self.first_run = True
+		self.setup_webdriver()
 
 	#-----------------------------------------------------------------
 
@@ -40,32 +42,41 @@ class Shift_Grabber:
 		"""
 		# Initializing driver options
 		fireFoxOptions = selenium.webdriver.FirefoxOptions()
-		if self.debug:
+		if self.headless:
 			# Prevent showing browser window
 			fireFoxOptions.add_argument('--headless')
 
 		# Create webdriver and add specified runtime arguments
-		driver = selenium.webdriver.Firefox(options=fireFoxOptions)
+		self.driver = selenium.webdriver.Firefox(options=fireFoxOptions)
+		return True
+	#-----------------------------------------------------------------
 
+	def save_cookies(self) -> bool:
+		self.driver.get(self.shift_pool_url)
+		time.sleep(60)
+		pickle.dump(self.driver.get_cookies() , open("cookies.pkl","wb"))
+		return True
+
+
+	def add_cookies(self) -> bool:
 		# Pre-load 7shifts url in order to add cookies (url must match cookie url)
-		driver.get(self.SHIFT_POOL_URL)
+		self.driver.get(self.shift_pool_url)
 
 		# Load login cookies
 		cookies = pickle.load(open("cookies.pkl", "rb"))
 
-		# Add each cookie to the current driver 
-		for cookie in cookies:
-		    driver.add_cookie(cookie)
-
-		return driver
+		# Add each cookie to the current driver
+		try:
+			for cookie in cookies:
+				self.driver.add_cookie(cookie)
+				True
+		except:
+			print('Adding cookie failed: Web address and cookie address must match')
+			return False
 
 	#-----------------------------------------------------------------
-	def save_cookies(self):
-		self.driver.get("https://app.7shifts.com/users/login?redirect=/company/139871/shift_pool/up_for_grabs")
-		time.sleep(35)
-		pickle.dump(self.driver.get_cookies() , open("cookies.pkl","wb"))
 
-	def get_shift_table(self):
+	def get_shift_table(self) -> list | bool:
 		"""
 		Attempting to retrieve html table containing individual shift entries
 		"""
@@ -74,36 +85,44 @@ class Shift_Grabber:
 		try:
 		    WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, "._1d8Ci")))
 		    table = self.driver.find_elements(By.CSS_SELECTOR, self.shift_table_selector);
-		    return table
+		    return list(table)
 		except:
-		    return False
+			print(table)
+			return False
 
 	#-----------------------------------------------------------------
 
-	def check_available_days(self,shift):
-		"""
-		Checking for open shifts and available dates for the specified position type
-		"""
-		if self.shift_wanted['position'].lower() in shift.text.lower():
-			shift_columns = shift.find_elements(By.CSS_SELECTOR, self.shift_details_selector);
-			for column in shift_columns[:-3]:
-				
-				if column.text[:3] == self.shift_wanted['day']:
+	def parse_shift(self, shift:list) -> dict:
+		# Labels to be used in attribute dictionaries
+		detail_labels = ['shift_poster','position', 'date', 'location', 'shift_type', 'position', 'button_label']
+		date_labels = ['day_week', 'month', 'day_month', 'year', 'clock_in', 'clock_out']
 
-					return column
+		# Select all of the html text elements within the shift html table
+		shift_details = shift.find_elements(By.CSS_SELECTOR, self.shift_details_selector)
+
+		# Create dict of labels and shift attributes
+		shift_details = {detail_labels[i]:shift_details[i].text for i in range(len(detail_labels))}
+
+		# Format shifts time
+		shift_details['date'] = shift_details['date'].replace(',','').replace(' -','').split(' ')
+
+		# Convert shifts date details into a dict of accessable date traits
+		shift_details['date'] = dict(zip(date_labels, shift_details['date']))
+		return shift_details
 
 	#-----------------------------------------------------------------
 
-	def pickup_shift(self,shift):
+	def pickup_shift(self, shift:list) -> dict:
 		"""
 		Attempts to find the first available shift for specified position type and day
 		"""
 		try:
-			OPEN_SHIFT = shift.find_element(By.TAG_NAME, self.shift_pickup_button)
-			OPEN_SHIFT.send_keys(Keys.RETURN)
+			open_shift = shift.find_element(By.TAG_NAME, self.shift_pickup_button)
+			open_shift.send_keys(Keys.RETURN)
 			"""DONT SEND A CLICK UNLESS SHIFT PICKUP IS INTENDED!!!!"""
-			pickup_button = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[11]/div/div/div/div[3]/div/button[1]')))
-			# REPLACE THIS LINE WITH .click() EVENT ON THE PICKUP BUTTON WHEN READY TO TAKE SHIFTS
+			# pickup_button = WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.XPATH, self.CONFIRM_PICKUP_BUTTON)))
+			# pickup_button.send_keys(Keys.RETURN)
+			# UNCOMMENT ABOVE LINE WHEN READY TO TAKE SHIFTS
 			"""*****************************************************"""
 			return True
 		except:
@@ -111,19 +130,40 @@ class Shift_Grabber:
 			return False
 
 	#-----------------------------------------------------------------
-	def check_available_times(self,shift_data):
-			
-		shift_data = shift_data.text.strip().split(' ')
 
-		shift_times = [data for data in shift_data if ":" in data]
+	def check_shift_location(self, shift_details:dict) -> bool:
+		if shift_details['location'] == self.shift_wanted['location']:
+			return True
 
-		shift_time = '-'.join(shift_times)
-
-		if shift_time == self.shift_wanted['time']:
+	def check_shift_day(self, shift_details:dict) -> bool:
+		"""
+		Checking for open shifts and available dates for the specified position type
+		"""
+		if shift_details['date']['day_week'] == self.shift_wanted['day']:
 			return True
 
 
-	def run(self):
+	def check_shift_time(self, shift_details:dict) -> bool:
+		shift_time = [
+			shift_details['date']['clock_in'],
+			shift_details['date']['clock_out']
+		]
+		if shift_time == self.shift_wanted['time']:
+			return True
+
+	#-----------------------------------------------------------------
+
+	def stop_webdriver(self) -> bool:
+		try:
+			self.driver.close()
+			return True
+		except:
+			print("Closing webdriver failed. Webdriver already closed.")
+			return False
+
+	#-----------------------------------------------------------------
+
+	def run(self) -> bool:
 		"""
 		Bots driver code.
 			-load 7shifts shift pool
@@ -132,53 +172,74 @@ class Shift_Grabber:
 			-pick up shift
 			-send user new shift info via telegram
 		"""
-		print(f"Searching available {self.shift_wanted['position']} shifts for: {self.shift_wanted['day']}")
+		print(f"Searching available {self.shift_wanted['position'].upper()} shifts for: {self.shift_wanted['day'].upper()}")
 
-		# Open the page showing available shifts
-		self.driver.get(self.SHIFT_POOL_URL)
+		# Direct webdriver to available shifts url
+		self.driver.get(self.shift_pool_url)
+
+		if self.first_run == True:
+			# Now that the webdrivers current url and the cookies url match the cookies may be added
+			self.add_cookies()
+			# Reload page with added cookies
+			self.driver.get(self.shift_pool_url)
+			self.first_run = False
 
 		# Process page elements into a list of found shifts
-		shifts = list(self.get_shift_table())
-
-		# Restart the loop if no shifts are up for grabs
-		if not len(shifts):
+		try:
+			found_shifts = self.get_shift_table()
+		except:
+			# Restart the loop if no shifts are up for grabs
+			print('Shift Pool Empty')
 			return False
+
 		# Look at all found shifts
-		for shift in shifts:
-			# Match a shift to the users requested day
-			shift_data = self.check_available_days(shift)
-			#If a shift is on that day
-			if shift_data:
-				# Check if the shifts time matches the users requested time
-				if self.check_available_times(shift_data):
-					# If the bot clicks the shift pickup button
-					if self.pickup_shift(shift):
-						print('Shift Picked Up:')
-						# Set message with found shifts information
-						self.telegram_bot_message = "\t"+shift_data.text+"\n"
-						return True
+		for shift in found_shifts:
+			shift_details = self.parse_shift(shift)
+			requested_location_found = self.check_shift_location(shift_details)
+
+			# If the shift day matches the requested day
+			if requested_location_found:
+				requested_day_found = self.check_shift_day(shift_details)
+
+				# If the shift day matches the requested day
+				if requested_day_found:
+					requested_time_found = self.check_shift_time(shift_details)
+
+					# If the shift time matches the requested time
+					if requested_time_found:
+
+						# If the bot successfully clicks the shift pickup button
+						if self.pickup_shift(shift):
+							print('Shift Picked Up:')
+							pprint(shift_details)
+							return True
 		else:
 			return False
-
 	#-----------------------------------------------------------------
 
 #*************************************
 
 # Main Driver Code
 if __name__ == '__main__':
-	# Initialize new scraper instance
-	scraper = Shift_Grabber()
+	# Load environment variables
+	load_dotenv()
 
-	# Will continue to check for open shifts until all desired shifts are grabbed.
+	shift_pool_url = os.getenv('SHIFT_POOL_URL')
+	CONFIRM_PICKUP_BUTTON = os.getenv('CONFIRM_PICKUP_BUTTON')
+
+	# Initialize scraper instance
+	scraper = Shift_Grabber(shift_pool_url=shift_pool_url, CONFIRM_PICKUP_BUTTON=CONFIRM_PICKUP_BUTTON)
+
+	# if scraper.save_cookies():
+	# 	print('login cookies saved')
+	# 	exit()
+
+	# Continues to scrape for the requested shift until it's picked up
 	while not scraper.shift_taken:
-		# scraper.save_cookies()
+		scraper.shift_taken = scraper.run()
 
-		# If the scraper picks up a shift
-		if scraper.run():
-			print(scraper.telegram_bot_message)
-			# Send shift details to users telegram account
-			telegram_bot.send_message('Shift picked up, check your schedule!'+'\n'+scraper.telegram_bot_message)
-			# Break the loop
-			scraper.shift_taken = True
+	# # If the scraper picks up a shift send sms notification to user
+	# twilio_sms.send_sms()
+
 	# Close the headless browser and ending session
-	scraper.driver.close()
+	scraper.stop_webdriver()
--- twilio_sms.py
+++ twilio_sms.py
@@ -10,12 +10,16 @@ TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_MESSAGING_SERVICE_SID = os.getenv('TWILIO_MESSAGING_SERVICE_SID')
 ACCOUNT_PHONE_NUMBER = os.getenv('ACCOUNT_PHONE_NUMBER')
 
-def send_sms(number=ACCOUNT_PHONE_NUMBER, message='success'):
+def send_sms(number=ACCOUNT_PHONE_NUMBER, message='shift picked up'):
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN) 
-     
-    message = client.messages.create(  
-                messaging_service_sid = TWILIO_MESSAGING_SERVICE_SID,       
-                to = number,
-                body = message
-            ) 
-    print(message.sid)
+    
+    try:
+        message = client.messages.create(  
+                    messaging_service_sid = TWILIO_MESSAGING_SERVICE_SID,       
+                    to = number,
+                    body = message
+                ) 
+        print('SMS SENT')
+
+    except:
+        print('SMS FAILED')
