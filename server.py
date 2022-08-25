19b71fa41958a5318732022c4653f802afd1d11d
heath <charleshparmley@icloud.com>
2022-08-25 17:42:29 -0500

    Nginx, HTTPS -- enabled

--- server.py
+++ server.py
@@ -112,5 +112,5 @@ def webhook():
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=5007, debug=True)
 	
