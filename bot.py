b6b49f4c07b09f128ab6295796f82db9b101f205
chparmley <63277749+chparmley@users.noreply.github.com>
2022-11-30 00:16:48 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,7 +147,7 @@ class Shift_Bot:
 			return False
 
 	#-----------------------------------------------------------------
-	def append_arrow_string(self, input_string, space_after=15):
+	def append_arrow_string(self, input_string, space_after=6):
 			return input_string + ' '*(space_after-len(input_string))
 
 	#-----------------------------------------------------------------		
@@ -185,6 +185,7 @@ class Shift_Bot:
 
 	def capitalize_string(self, input_string):
 		return ' '.join([word.capitalize() for word in input_string.split(' ')])
+
 	def format_shift_message(self, shift_details):
 		shift_detail_string = f" \
 		{shift_details['position'].capitalize()} \
