c00cb0c77d9f4974d6d9bdb10b036c07e1f6e3d3
chparmley <63277749+chparmley@users.noreply.github.com>
2022-11-29 19:30:27 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,7 +165,7 @@ class Shift_Bot:
 		shift_details = {detail_labels[i]:shift_details[i].text.lower() for i in range(len(detail_labels))}
 		
 		# Format shift posters name
-		shift_details['shift_poster'] = f'{[name.capitalize() for name in shift_details['shift_poster'].split()]}'
+		shift_details['shift_poster'] = ' '.join([name.capitalize() for name in shift_details['shift_poster'].split()])
 		
 		# Format shift location
 		shift_details['location'] = ' '.join([location_name.capitalize() for location_name in shift_details['location'].split()])
