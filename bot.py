3951fa44a35ea847c48d93ec518092d252a20ede
chparmley <63277749+chparmley@users.noreply.github.com>
2022-11-29 21:11:49 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -188,8 +188,9 @@ class Shift_Bot:
 		return ' '.join([word.capitalize() for word in input_string.split(' ')])
 	def format_shift_message(self, shift_details):
 		shift_detail_string = f"\t{self.capitalize_string(shift_details['shift_poster'])} \
+		\n\t{shift_details['position'].capitalize()}\n\t{shift_details['date']['day_week'].capitalize()}\
 		\n\t{self.capitalize_string(shift_details['locations'])}\
-		\n\t{shift_details['position']}\n\t{shift_details['date']['day_week'].capitalize()}:{shift_details['date']['clock_in']}-{shift_details['date']['clock_out']}\n"
+		{shift_details['date']['clock_in']}-{shift_details['date']['clock_out']}\n"
 		return shift_detail_string
 	#-----------------------------------------------------------------
 
