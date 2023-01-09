bcfb6547d8571de65951fc41623930c9541328de
chparmley <charleshparmley@icloud.com>
2023-01-09 17:35:25 -0600

    k

--- v3/server.py
+++ v3/server.py
@@ -110,15 +110,15 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('index'))
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, password=form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for('login'))
-#     return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 # *******************************************************************************
 class RegistrationForm(FlaskForm):
