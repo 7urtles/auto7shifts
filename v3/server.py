d19fccda06b69bc83f94e3f5ab93f28bcb2f9ee9
chparmley <charleshparmley@icloud.com>
2023-02-11 19:01:44 -0600

    branching

--- v3/server.py
+++ v3/server.py
@@ -114,49 +114,57 @@ def shift_selector(pool_data:list[dict]) -> bool:
 	return False
 # ---------------------------------------------------------------------------
 
+"""
+# WIP
+# for web ui access and interface
+
 @app.route('/')
 def index():
-    # if not session.get('logged_in'):
-    #     return redirect(url_for('login'))
-    # form = RegistrationForm()
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    form = RegistrationForm()
     return render_template("index.html")
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user and user.password == form.password.data:
-#             session['logged_in'] = True
-#             return redirect(url_for('index'))
-#     return render_template('login.html', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
-# @app.route('/logout')
-# def logout():
-#     session.pop('logged_in', None)
-#     return redirect(url_for('index'))
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, password=form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for('login'))
-#     return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+---------------------------------------------------------------------------
 
-# *******************************************************************************
-# class RegistrationForm(FlaskForm):
-#     email = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-#     submit = SubmitField('Sign Up')
 
-# class LoginForm(FlaskForm):
-#     email = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Log In')
+class RegistrationForm(FlaskForm):
+    email = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+---------------------------------------------------------------------------
+"""
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
